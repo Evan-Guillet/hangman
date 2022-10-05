ba5f3d79afd40be450d9810bdaa6ae9dee54ad0b
smathis <mathis.silotia@ynov.com>
2022-10-05 16:59:12 +0200

    resolve bug underscore

--- chooseWord.go
+++ chooseWord.go
@@ -10,5 +10,16 @@ func ChooseWord(fileIncome []byte) string {
     file := strings.Split(string(fileIncome), "\n")
     rand.Seed(time.Now().UnixNano())
     randWord := rand.Intn(len(file))
-    return file[randWord]
+    wordRune := []rune(file[randWord])
+    var word []rune
+    var wordString string
+    for i := 0; i < len(wordRune); i++ {
+        if wordRune[i] >= 'a' && wordRune[i] <= 'z' {
+            word = append(word, wordRune[i])
+        }
+    }
+    for _, letter := range word {
+        wordString += string(letter)
+    }
+    return wordString
 }
