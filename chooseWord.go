1747fbf85301dd91fc94e7b0f8c7341e69903546
smathis <mathis.silotia@ynov.com>
2022-11-05 10:29:27 +0100

    add comment in chooseword.go

--- chooseWord.go
+++ chooseWord.go
@@ -10,13 +10,13 @@ import (
 )
 
 func ChooseWord(fileIncome []byte) string {
-	file := strings.Split(string(fileIncome), "\n")
+	file := strings.Split(string(fileIncome), "\n") //split each word in a string table
 	rand.Seed(time.Now().UnixNano())
-	randWord := rand.Intn(len(file))
+	randWord := rand.Intn(len(file)) //choose a random index of the table
 	wordRune := []rune(file[randWord])
 	var word []rune
 	var wordString string
-	for i := 0; i < len(wordRune); i++ {
+	for i := 0; i < len(wordRune); i++ { //this loop prevent error from special character
 		if wordRune[i] >= 'a' && wordRune[i] <= 'z' {
 			word = append(word, wordRune[i])
 		}
@@ -27,7 +27,7 @@ func ChooseWord(fileIncome []byte) string {
 			word = append(word, 'c')
 		}
 	}
-	for _, letter := range word {
+	for _, letter := range word { //convert the rune word into a string
 		wordString += string(letter)
 	}
 	return wordString
@@ -35,7 +35,7 @@ func ChooseWord(fileIncome []byte) string {
 
 func AddWord (fileIncome []byte) {
 	isValid := true
-	list := strings.Split(string(fileIncome), "\n")
+	list := strings.Split(string(fileIncome), "\n") //split each word in a string table
 	if conclusion {
 		fmt.Print("Do you want to add a word to the list ? (yes/no): ")
 		reader := bufio.NewReader(os.Stdin)
@@ -43,7 +43,7 @@ func AddWord (fileIncome []byte) {
 		add = strings.Replace(add,"\n","",-1)
 		
 		if add == "yes" {
-			f,_ := os.OpenFile("../words.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			f,_ := os.OpenFile("../words.txt", os.O_APPEND|os.O_WRONLY, 0644) //open the file to read and write
 
 			fmt.Print("choose a word : ")
 			reader := bufio.NewReader(os.Stdin)
@@ -53,7 +53,7 @@ func AddWord (fileIncome []byte) {
 
 			char := []rune(word)
 			
-			for j := 0; j < len(char); j++ {
+			for j := 0; j < len(char); j++ { //this loop prevent add of invalid character
 				if (char[j] != 'é' && char[j] != 'è') && (char[j] < 'a' || char[j] > 'z') {
 					isValid = false
 					fmt.Println("You entered an invalid character")
@@ -61,7 +61,7 @@ func AddWord (fileIncome []byte) {
 			}
 			
 			if isValid {
-				for i := 0; i < len(list); i++ {
+				for i := 0; i < len(list); i++ { // verif if the word is already in the list
 					if word == list[i] {
 						fmt.Println("This word is already in the list")
 						isValid = false
@@ -73,7 +73,7 @@ func AddWord (fileIncome []byte) {
 			}
 			if isValid {
 				fmt.Fprint(f,"\n")
-				fmt.Fprint(f,word)
+				fmt.Fprint(f,word) //add the word to the list
 			} else {
 				AddWord(fileIncome)
 			}
